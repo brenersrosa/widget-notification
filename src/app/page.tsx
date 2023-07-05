c2dd087e713da324ef05e267ed324c50ddb09a8f
Brener Rosa <brenersrosa@gmail.com>
2023-07-05 04:31:40 -0300

    fix: update page.tsx with prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Widget } from '@/components/Widget'
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-6 h-screen items-center justify-center bg-zinc-50 dark:bg-gray-950 text-zinc-700 dark:text-zinc-50">
+    <main className="flex h-screen flex-col items-center justify-center gap-6 bg-zinc-50 text-zinc-700 dark:bg-gray-950 dark:text-zinc-50">
       <ThemeSwitcher />
       <Widget />
     </main>
